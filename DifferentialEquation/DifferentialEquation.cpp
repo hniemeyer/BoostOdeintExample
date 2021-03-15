a3468c02ec883d58a1a174eb76f0330bc36244d3
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-03-15 11:52:44 +0100

    clang format document

--- DifferentialEquation/DifferentialEquation.cpp
+++ DifferentialEquation/DifferentialEquation.cpp
@@ -15,23 +15,23 @@ auto create_iterators(Container &state, Stepper &stepper, Func &func, double t0,
 }
 
 template <typename Container, typename Func>
-void solve_and_print(Container& state, Func& func, double t0, double tmax, double dt)
-{
-	boost::numeric::odeint::runge_kutta4<Container> stepper;
-	
-	auto [ode_begin_iterator, ode_end_iterator] =
-		create_iterators(state, stepper, func, t0, tmax, dt);
-	for (auto ode_iterator = ode_begin_iterator; ode_iterator != ode_end_iterator;
-	     ++ode_iterator) {
-		const auto &[current_state, time] = *ode_iterator;
-		fmt::print("t = {} x = {} p = {}\n", time, current_state[0],
-		           current_state[1]);
-	}
+void solve_and_print(Container &state, Func &func, double t0, double tmax,
+                     double dt) {
+  boost::numeric::odeint::runge_kutta4<Container> stepper;
+
+  auto [ode_begin_iterator, ode_end_iterator] =
+      create_iterators(state, stepper, func, t0, tmax, dt);
+  for (auto ode_iterator = ode_begin_iterator; ode_iterator != ode_end_iterator;
+       ++ode_iterator) {
+    const auto &[current_state, time] = *ode_iterator;
+    fmt::print("t = {} x = {} p = {}\n", time, current_state[0],
+               current_state[1]);
+  }
 }
 
 int main() {
   std::vector<double> state{0.0, 1.0};
-  
+
   const auto harmonic_oscillator = [gamma = 0.15](const std::vector<double> &x,
                                                   std::vector<double> &dxdt,
                                                   double /* t */) {
