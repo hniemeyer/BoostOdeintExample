7edb1fa29e60695634f04b74d8d09716628ca1d4
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-03-15 11:46:49 +0100

    refactored creating the solver into its own function

--- DifferentialEquation/DifferentialEquation.cpp
+++ DifferentialEquation/DifferentialEquation.cpp
@@ -14,22 +14,30 @@ auto create_iterators(Container &state, Stepper &stepper, Func &func, double t0,
       make_const_step_time_iterator_end(stepper, func, state));
 }
 
+template <typename Container, typename Func>
+void solve_and_print(Container& state, Func& func, double t0, double tmax, double dt)
+{
+	boost::numeric::odeint::runge_kutta4<std::vector<double>> stepper;
+	
+	auto [ode_begin_iterator, ode_end_iterator] =
+		create_iterators(state, stepper, func, t0, tmax, dt);
+	for (auto ode_iterator = ode_begin_iterator; ode_iterator != ode_end_iterator;
+	     ++ode_iterator) {
+		const auto &[current_state, time] = *ode_iterator;
+		fmt::print("t = {} x = {} p = {}\n", time, current_state[0],
+		           current_state[1]);
+	}
+}
+
 int main() {
   std::vector<double> state{0.0, 1.0};
-  boost::numeric::odeint::runge_kutta4<std::vector<double>> stepper;
+  
   const auto harmonic_oscillator = [gamma = 0.15](const std::vector<double> &x,
                                                   std::vector<double> &dxdt,
-                                                  double t) {
+                                                  double /* t */) {
     dxdt[0] = x[1];
     dxdt[1] = -x[0] - gamma * x[1];
   };
 
-  auto [ode_begin_iterator, ode_end_iterator] =
-      create_iterators(state, stepper, harmonic_oscillator, 0.0, 10.0, 0.1);
-  for (auto ode_iterator = ode_begin_iterator; ode_iterator != ode_end_iterator;
-       ++ode_iterator) {
-    const auto &[current_state, time] = *ode_iterator;
-    fmt::print("t = {} x = {} p = {}\n", time, current_state[0],
-               current_state[1]);
-  }
+  solve_and_print(state, harmonic_oscillator, 0.0, 10.0, 0.1);
 }
